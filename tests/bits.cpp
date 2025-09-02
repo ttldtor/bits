15ea89f78abaac9823a452656168e3ebf40436e9
ttldtor <ttldtor@gmail.com>
2025-09-02 13:02:47 +0300

    Add tests' skeleton

---
+++ tests/bits.cpp
@@ -0,0 +1,9 @@
+﻿// Copyright (c) 2025 ttldtor.
+// SPDX-License-Identifier: BSL-1.0
+
+#include <doctest/doctest.h>
+#include <bits/bits.hpp>
+
+TEST_CASE("simple check") {
+    CHECK(42 == 42);
+}
--- xmake.lua
+++ xmake.lua
@@ -6,3 +6,14 @@ add_rules("mode.debug", "mode.release")
 target("libs")
     set_kind("headeronly")
     add_includedirs("include", {public = true})
+
+option("tests", {default = true, showmenu = true})
+
+if has_config("tests") then
+    add_requires("doctest")
+    target("bits_tests")
+        set_kind("binary")
+        add_files("tests/*.cpp")
+        add_packages("doctest")
+        add_deps("bits")
+end
