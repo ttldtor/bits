8f3d3acaf6e49d07160c65147e0b16aab8ad547c
ttldtor <ttldtor@gmail.com>
2025-09-02 21:35:54 +0300

    Add usage examples in README and `xmake.lua` package definition

--- README.md
+++ README.md
@@ -1,3 +1,37 @@
 # bits
 
 Library for bit manipulation.
+
+## Examples of usage
+
+### xmake
+
+```lua
+add_requires("bits default", { 
+    urls = "https://github.com/ttldtor/bits.git" 
+})
+
+target("myapp")
+    set_kind("binary")
+    add_files("src/main.cpp")
+    add_packages("bits")
+```
+
+### CMake
+
+```cmake
+cmake_minimum_required(VERSION 3.16)
+project(myapp LANGUAGES CXX)
+
+include(FetchContent)
+FetchContent_Declare(
+  bits
+  GIT_REPOSITORY https://github.com/ttldtor/bits.git
+  GIT_TAG        default
+)
+FetchContent_MakeAvailable(bits)
+
+add_executable(myapp main.cpp)
+target_link_libraries(myapp PRIVATE bits)
+```
+
--- xmake.lua
+++ xmake.lua
@@ -1,11 +1,25 @@
 ﻿-- Copyright (c) 2025 ttldtor.
 -- SPDX-License-Identifier: BSL-1.0
 
+package("bits")
+    set_kind("library", {headeronly = true})
+    set_homepage("https://github.com/ttldtor/bits")
+    set_description("Library for bit manipulation")
+    set_license("BSL-1.0")
+
+    add_urls("https://github.com/ttldtor/bits.git")
+
+    on_install(function (package)
+        os.cp("include", package:installdir())
+    end)
+package_end()
+
 add_rules("mode.debug", "mode.release")
 
-target("libs")
+target("bits")
     set_kind("headeronly")
     add_includedirs("include", {public = true})
+    set_languages("c++20")
 
 option("tests", {default = true, showmenu = true})
 
@@ -14,6 +28,7 @@ if has_config("tests") then
     target("bits_tests")
         set_kind("binary")
         add_files("tests/*.cpp")
+        set_languages("c++20")
         add_packages("doctest")
         add_deps("bits")
 end
