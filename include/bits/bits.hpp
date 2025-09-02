cf68570f55e316652908d96e3a72e48ecfeac615
ttldtor <ttldtor@gmail.com>
2025-09-02 16:14:17 +0300

    Refactor bitwise operations to use helper functions (`andOp`, `orOp`, `xorOp`) and improve clarity in `shift` operations.

--- include/bits/bits.hpp
+++ include/bits/bits.hpp
@@ -30,61 +30,6 @@ struct MaxImpl<T, U, Ts...> {
 template <typename... Ts>
 using Max = detail::MaxImpl<Ts...>::Type;
 
-template <std::unsigned_integral T>
-static constexpr T bitsAreSet(T sourceBits, T bitMaskToCheck) {
-  return (sourceBits & bitMaskToCheck) != 0;
-}
-
-template <std::integral SB, std::integral M>
-static constexpr SB bitsAreSet(SB sourceBits, M bitMaskToCheck) {
-  using MaxType = Max<SB, M>;
-
-  if constexpr (std::is_signed_v<SB> || std::is_signed_v<M>) {
-    using U = std::make_unsigned_t<MaxType>;
-
-    return static_cast<SB>(bitsAreSet(static_cast<U>(sourceBits), static_cast<U>(bitMaskToCheck)));
-  } else {
-    return static_cast<SB>(bitsAreSet(static_cast<MaxType>(sourceBits), static_cast<MaxType>(bitMaskToCheck)));
-  }
-}
-
-template <std::unsigned_integral T>
-static constexpr T setBits(T sourceBits, T bitMaskToSet) {
-  return sourceBits | bitMaskToSet;
-}
-
-template <std::integral SB, std::integral M>
-static constexpr SB setBits(SB sourceBits, M bitMaskToSet) {
-  using MaxType = Max<SB, M>;
-
-  if constexpr (std::is_signed_v<SB> || std::is_signed_v<M>) {
-    using U = std::make_unsigned_t<MaxType>;
-
-    return static_cast<SB>(setBits(static_cast<U>(sourceBits), static_cast<U>(bitMaskToSet)));
-  } else {
-    return static_cast<SB>(setBits(static_cast<MaxType>(sourceBits), static_cast<MaxType>(bitMaskToSet)));
-  }
-}
-
-template <std::unsigned_integral T>
-static constexpr T resetBits(T sourceBits, T bitMaskToReset) {
-  return sourceBits & ~bitMaskToReset;
-}
-
-template <std::integral SB, std::integral M>
-// ReSharper disable once CppDFAConstantParameter
-static constexpr SB resetBits(SB sourceBits, M bitMaskToReset) {
-  using MaxType = Max<SB, M>;
-
-  if constexpr (std::is_signed_v<SB> || std::is_signed_v<M>) {
-    using U = std::make_unsigned_t<MaxType>;
-
-    return static_cast<SB>(resetBits(static_cast<U>(sourceBits), static_cast<U>(bitMaskToReset)));
-  } else {
-    return static_cast<SB>(resetBits(static_cast<MaxType>(sourceBits), static_cast<MaxType>(bitMaskToReset)));
-  }
-}
-
 /**
  * Performs a right arithmetic bit shift operation (>> in Java, C, etc.). The sign bit is extended to preserve the
  * signedness of the number.
@@ -260,7 +205,9 @@ static constexpr V leftLogicalShift(V value, S shift) noexcept {
 
   if constexpr (std::is_signed_v<S>) {
     if (shift < 0) {
-      return shr(value, -shift);
+      const auto magnitude = UnsignedShift{} - static_cast<UnsignedShift>(shift);
+
+      return shr(value, magnitude);
     }
   }
 
@@ -268,13 +215,14 @@ static constexpr V leftLogicalShift(V value, S shift) noexcept {
     return value;
   }
 
-  auto unsignedShift = static_cast<std::make_unsigned_t<S>>(shift);
+  const auto unsignedShift = static_cast<UnsignedShift>(shift);
+  const auto bitWidth = static_cast<UnsignedShift>(sizeof(V) * CHAR_BIT);
 
-  if (unsignedShift >= sizeof(V) * CHAR_BIT) {
-    return 0;
+  if (unsignedShift >= bitWidth) {
+    return V{0};
   }
 
-  return value << unsignedShift;
+  return static_cast<V>(value << unsignedShift);
 }
 
 /**
@@ -312,9 +260,13 @@ static constexpr V shl(V value, S shift) noexcept {
  */
 template <std::integral V, std::integral S>
 static constexpr V rightLogicalShift(V value, S shift) noexcept {
+  using UnsignedShift = std::make_unsigned_t<S>;
+
   if constexpr (std::is_signed_v<S>) {
     if (shift < 0) {
-      return shl(value, -shift);
+      const auto magnitude = UnsignedShift{} - static_cast<UnsignedShift>(shift);
+
+      return shl(value, magnitude);
     }
   }
 
@@ -322,13 +274,14 @@ static constexpr V rightLogicalShift(V value, S shift) noexcept {
     return value;
   }
 
-  auto unsignedShift = static_cast<std::make_unsigned_t<S>>(shift);
+  const auto unsignedShift = static_cast<UnsignedShift>(shift);
+  const auto bitWidth = static_cast<UnsignedShift>(sizeof(V) * CHAR_BIT);
 
-  if (unsignedShift >= sizeof(V) * CHAR_BIT) {
-    return 0;
+  if (unsignedShift >= bitWidth) {
+    return V{0};
   }
 
-  return static_cast<V>(static_cast<std::make_unsigned_t<V>>(value) >> unsignedShift);
+  return static_cast<V>(static_cast<UnsignedShift>(value) >> unsignedShift);
 }
 
 /**
@@ -350,6 +303,16 @@ static constexpr V shr(V value, S shift) noexcept {
   return rightLogicalShift(value, shift);
 }
 
+/**
+ * Performs a bitwise AND operation between two values of possibly different types
+ * and ensures the result is cast back to the type of the first argument.
+ *
+ * @tparam A The type of the first argument.
+ * @tparam B The type of the second argument.
+ * @param a The first operand of type A.
+ * @param b The second operand of type B.
+ * @return The result of the bitwise AND operation, cast to type A.
+ */
 template <std::integral A, std::integral B>
 static constexpr A andOp(A a, B b) noexcept {
   using Common = std::make_unsigned_t<Max<A, B>>;
@@ -357,6 +320,16 @@ static constexpr A andOp(A a, B b) noexcept {
   return static_cast<A>(static_cast<Common>(a) & static_cast<Common>(b));
 }
 
+/**
+ * Performs a bitwise OR operation between two values of possibly different types
+ * and ensures the result is cast back to the type of the first argument.
+ *
+ * @tparam A The type of the first argument.
+ * @tparam B The type of the second argument.
+ * @param a The first operand of type A.
+ * @param b The second operand of type B.
+ * @return The result of the bitwise OR operation, cast to type A.
+ */
 template <std::integral A, std::integral B>
 static constexpr A orOp(A a, B b) noexcept {
   using Common = std::make_unsigned_t<Max<A, B>>;
@@ -364,11 +337,127 @@ static constexpr A orOp(A a, B b) noexcept {
   return static_cast<A>(static_cast<Common>(a) | static_cast<Common>(b));
 }
 
+/**
+ * Performs a bitwise XOR operation between two values of possibly different types
+ * and ensures the result is cast back to the type of the first argument.
+ *
+ * @tparam A The type of the first argument.
+ * @tparam B The type of the second argument.
+ * @param a The first operand of type A.
+ * @param b The second operand of type B.
+ * @return The result of the bitwise XOR operation, cast to type A.
+ */
 template <std::integral A, std::integral B>
 static constexpr A xorOp(A a, B b) noexcept {
   using Common = std::make_unsigned_t<Max<A, B>>;
 
   return static_cast<A>(static_cast<Common>(a) ^ static_cast<Common>(b));
+}
+
+/**
+ * Determines if the specified bits are set in the source value.
+ *
+ * @tparam T the type of arguments.
+ * @param sourceBits The value to check for the presence of specific bits.
+ * @param bitMaskToCheck The mask of bits to check against the source value.
+ * @return `true` if the specified bits are set in the source value.
+ */
+template <std::unsigned_integral T>
+static constexpr T bitsAreSet(T sourceBits, T bitMaskToCheck) {
+  return andOp(sourceBits, bitMaskToCheck) != 0;
+}
+
+/**
+ * Determines if the specified bits are set in the source value.
+ *
+ * @tparam SB The type of the source bits (e.g., an integer type).
+ * @tparam M The type of the bit mask (e.g., an integer type).
+ * @param sourceBits The value to check for the presence of specific bits.
+ * @param bitMaskToCheck The mask of bits to check against the source value.
+ * @return `true` if the specified bits are set in the source value.
+ */
+template <std::integral SB, std::integral M>
+static constexpr SB bitsAreSet(SB sourceBits, M bitMaskToCheck) {
+  using MaxType = Max<SB, M>;
+
+  if constexpr (std::is_signed_v<SB> || std::is_signed_v<M>) {
+    using U = std::make_unsigned_t<MaxType>;
+
+    return static_cast<SB>(bitsAreSet(static_cast<U>(sourceBits), static_cast<U>(bitMaskToCheck)));
+  } else {
+    return static_cast<SB>(bitsAreSet(static_cast<MaxType>(sourceBits), static_cast<MaxType>(bitMaskToCheck)));
+  }
+}
+
+/**
+ * Sets specific bits in the source value using a bitmask.
+ *
+ * @tparam T The type of arguments.
+ * @param sourceBits The original bits to modify.
+ * @param bitMaskToSet The bitmask representing the bits to be set.
+ * @return The resulting value after setting the specified bits.
+ */
+template <std::unsigned_integral T>
+static constexpr T setBits(T sourceBits, T bitMaskToSet) {
+  return orOp(sourceBits, bitMaskToSet);
+}
+
+/**
+ * Sets specific bits in the source value using a bitmask.
+ *
+ * @tparam SB The type of the source bits (e.g., an integer type).
+ * @tparam M The type of the bit mask (e.g., an integer type).
+ * @param sourceBits The original bits to modify.
+ * @param bitMaskToSet The bitmask representing the bits to be set.
+ * @return The resulting value after setting the specified bits.
+ */
+template <std::integral SB, std::integral M>
+static constexpr SB setBits(SB sourceBits, M bitMaskToSet) {
+  using MaxType = Max<SB, M>;
+
+  if constexpr (std::is_signed_v<SB> || std::is_signed_v<M>) {
+    using U = std::make_unsigned_t<MaxType>;
+
+    return static_cast<SB>(setBits(static_cast<U>(sourceBits), static_cast<U>(bitMaskToSet)));
+  } else {
+    return static_cast<SB>(setBits(static_cast<MaxType>(sourceBits), static_cast<MaxType>(bitMaskToSet)));
+  }
+}
+
+/**
+ * Resets (clears) specific bits in a bitmask.
+ *
+ * @tparam T The type of the source bits and the bitmask. Should be an integral type.
+ * @param sourceBits The original bitmask containing the bits to modify.
+ * @param bitMaskToReset The bitmask specifying the bits to reset.
+ * @return The resulting value after resetting the specified bits.
+ */
+template <std::unsigned_integral T>
+static constexpr T resetBits(T sourceBits, T bitMaskToReset) {
+  return andOp(sourceBits, ~bitMaskToReset);
+}
+
+/**
+ * Resets (clears) specific bits in a bitmask.
+ *
+ * @tparam SB The type of the source bits (e.g., an integer type).
+ * @tparam M The type of the bit mask (e.g., an integer type).
+ * @param sourceBits The original bitmask containing the bits to modify.
+ * @param bitMaskToReset The bitmask specifying the bits to reset.
+ * @return The resulting value after resetting the specified bits.
+ */
+template <std::integral SB, std::integral M>
+// ReSharper disable once CppDFAConstantParameter
+static constexpr SB resetBits(SB sourceBits, M bitMaskToReset) {
+  using MaxType = Max<SB, M>;
+
+  if constexpr (std::is_signed_v<SB> || std::is_signed_v<M>) {
+    using U = std::make_unsigned_t<MaxType>;
+
+    return static_cast<SB>(resetBits(static_cast<U>(sourceBits), static_cast<U>(bitMaskToReset)));
+  } else {
+    return static_cast<SB>(resetBits(static_cast<MaxType>(sourceBits), static_cast<MaxType>(bitMaskToReset)));
+  }
 }
 
 }  // namespace org::ttldtor::bits
